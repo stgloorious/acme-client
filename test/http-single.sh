78f1b0e1274c8b89fef65eea0610205f973d6c6e
Stefan Gloor <code@stefan-gloor.ch>
2022-11-29 13:40:04 +0100

    Changing test port to 8080

--- test/http-single.sh
+++ test/http-single.sh
@@ -4,7 +4,7 @@ echo "Starting Pebble testing server"
 pebble -config "../../test/pebble-config.json" &
 sleep 5 # Wait for server to come up
 
-cmd="../acme-client --dir https://pebble:14000/dir --domain example.com --cert=../../test/pebble.minica.pem --agree-tos http01"
+cmd="../acme-client --dir https://pebble:14000/dir --domain example.com --cert=../../test/pebble.minica.pem --agree-tos http01 --port 8080"
 echo "Running $cmd"
 echo "$cmd" | bash
 status=$?
