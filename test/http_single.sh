f2cc8e020a9bb2f6fef530ceab95d9dcb21b2571
Stefan Gloor <code@stefan-gloor.ch>
2022-11-30 17:56:34 +0100

    Changed http_single test

--- test/http_single.sh
+++ test/http_single.sh
@@ -1,31 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Starting Pebble testing server"
 pebble -config "../../test/pebble-config.json" &
+pebble_pid=$!
 sleep 5 # Wait for server to come up
 
 cmd="../acme-client --dir https://pebble:14000/dir --domain example.com \
-        --cert ../../test/pebble.minica.pem --agree-tos http01 --port 8080"
+        --verbose --cert ../../test/pebble.minica.pem --agree-tos \
+        http01 --port 8080"
 echo "Running $cmd"
-echo "$cmd" | bash
+timeout 60 echo "$cmd" | bash
 status=$?
-
-# Obtain the root certificate of the certificate chain that we obtained 
-# through ACME
-curl https://pebble:15000/roots/0 --silent --cacert ../test/pebble.minica.pem -o root.pem
 
 if [[ $status -ne 0 ]]; then
         echo "Could not obtain certificate."
+        kill $pebble_pid
         exit -1
 fi
+
+# Obtain the root certificate of the certificate chain that we obtained 
+# through ACME
+curl https://pebble:15000/roots/0 --silent --cacert ../../test/pebble.minica.pem -o root.pem
+
+kill $pebble_pid
 
 # Start an HTTPS server with our newly obtained certificate
 cat client.key cert.crt > cert.pem
 python3 ../../test/https_server.py cert.pem &
+https_pid=$!
+echo "Started HTTPS server with PID $https_pid"
 sleep 1
 
 # Curl the HTTPS server to check the certificate
 curl https://example.com:5443 --cacert root.pem
 status=$?
 
+kill $https_pid
 exit $status
