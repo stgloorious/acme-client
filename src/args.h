8e4c1751ea0b393a87f797d9813ded5e41ac4d99
Stefan Gloor <code@stefan-gloor.ch>
2022-12-05 19:32:15 +0100

    Changed license header

--- src/args.h
+++ src/args.h
@@ -1,18 +1,19 @@
-/** 
- * @file args.h
+/* SPDX-License-Identifier: GPL-3.0-or-later
  *
- * This program is free software: you can redistribute it and/or modify it 
- * under the terms of the GNU General Public License as published by 
- * the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
+ * This file is part of acme-client.
  *
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty 
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * acme-client is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the 
+ * Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ *
+ * acme-client is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
  * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License 
- * along with this program. If not, see <https://www.gnu.org/licenses/>. 
+ * along with acme-client. If not, see <https://www.gnu.org/licenses/>. 
  *
  * Copyright 2022 Stefan Gloor
  *
